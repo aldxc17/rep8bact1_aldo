17b28cfa26bdd6025fae751365c2d142982e6c15
aldxc17 <aldcpp9@gmail.com>
2026-02-12 17:56:21 -0600

    Add index php file with initial HTML structure and welcome message

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rep8bact1_aldo
 repositorio para mostrar mis skills trabajando con git
+
+
+# Universidad Tecnológica de la Selva
+# Ocosingo, Chiapas, 2026
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aldo Page</title>
+</head>
+<body>
+    <h1>
+        Bienvenido a la página de Aldo
+    </h1>
+</body>
+</html>
